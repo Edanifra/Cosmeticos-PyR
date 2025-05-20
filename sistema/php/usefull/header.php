2db702bfdd867b0c5e6af9ed6778f972af708635
Elfresita10 <117861600+Elfresita10@users.noreply.github.com>
2025-05-19 21:43:53 -0400

    Carga inicial de la aplicación
    Subimos el proyecto completo a GitHub

---
+++ sistema/php/usefull/header.php
@@ -0,0 +1,23 @@
+<header class="header black-bg" id="header">
+      <div class="sidebar-toggle-box">
+          <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+      </div>
+    <!--logo start-->
+    <a href="../../vistas/dashboard/index.php" class="logo"><b>PyR Cosmetics</b></a>
+    <!--logo end-->
+    <div class="nav notify-row" id="top_menu">
+        <!--  notification start -->
+        <ul class="nav top-menu">
+    </div>
+    <div class="top-menu">
+    	<ul class="nav pull-right top-menu">
+            <li><a class="logout" href="../../salir.php"><i class="fa fa-sign-out" aria-hidden="true" style="font-size: 1.7em;"></i> Cerrar Sesión</a></li>
+    	</ul>
+    </div>
+</header>
+
+<div class="modal">
+    <div class="bodyModal">
+        
+    </div>
+</div>
